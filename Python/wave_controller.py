93babbdd02604257149072cbc35db31f18e11775
AlexisCogne <alexis.cogne@outlook.com>
2024-04-11 14:56:54 +0200

    Updating wave_frequency the correct way

--- Python/wave_controller.py
+++ Python/wave_controller.py
@@ -17,6 +17,9 @@ class WaveController:
             pars.timestep,
             pars.n_iterations)
         self.n_joints = pars.n_joints
+        self.A = pars.A
+        self.freq = pars.freq
+        self.epsilon = pars.epsilon
 
         # state array for recording all the variables
         self.state = np.zeros((pars.n_iterations, 2*self.n_joints))
@@ -29,7 +32,7 @@ class WaveController:
         # indexes of the right muscle activations (optional)
         self.muscle_r = self.muscle_l+1
 
-    def step(self, iteration, time, timestep, pos=None, epsilon = 1.25, A = 0.75, freq = 3):
+    def step(self, iteration, time, timestep, pos=None):
         """
         Step function. This function passes the activation functions of the muscle model
         Inputs:
@@ -48,27 +51,15 @@ class WaveController:
         """
         # The lines below with if iteration < x is to change the parameters during the simulation. 
         # This will be erased later but allows to test different parameters during the simulation and see effects.
-        if iteration < 5000:
-            A = 0.75
-            freq = 1
-            epsilon = 1.25
-
-        if iteration >= 5000 and time < 10000:
-            A = 0.75
-            freq = 3
-            epsilon = 1.25
-        if iteration > 20000:
-            A = 0.75
-            freq = 1
-            epsilon = 1.25
+        
         for i in range(self.n_joints):
             l_index = self.muscle_l[i]
             r_index = self.muscle_r[i]
             #print(f"i={i}, l_idx={l_index}, r_idx={r_index}")
             #self.state[iteration, l_index] = 0.5 
-            self.state[iteration, l_index] = 0.5 + A/2*np.sin(2*np.pi*(freq*time-epsilon*i/self.n_joints))
+            self.state[iteration, l_index] = 0.5 + self.A/2*np.sin(2*np.pi*(self.freq*time-self.epsilon*i/self.n_joints))
 
-            self.state[iteration, r_index] = 0.5 - A/2*np.sin(2*np.pi*(freq*time-epsilon*i/self.n_joints))
+            self.state[iteration, r_index] = 0.5 - self.A/2*np.sin(2*np.pi*(self.freq*time-self.epsilon*i/self.n_joints))
             #self.state[iteration, r_index] = 0.5
         return self.state[iteration, :]
 
