530b1d98710b246549a66009659d094527180dbf
AlexisCogne <alexis.cogne@outlook.com>
2024-04-17 13:11:07 +0200

    No changes

--- Python/example_multiple.py
+++ Python/example_multiple.py
@@ -29,7 +29,7 @@ def exercise_multiple():
             return_network = True
         )
         for i, amp in enumerate(np.linspace(0.05, 2, nsim))
-        for j, wavefrequency in enumerate(np.linspace(0., 0.1, nsim))
+        for j, wavefrequency in enumerate(np.linspace(0., 2, nsim))
     ]
 
     controller = run_multiple(pars_list, num_process=16)
@@ -37,12 +37,12 @@ def exercise_multiple():
         print(controller[k].metrics)
         print('---------------------------------')
 
-    # make array n by 3 corresponding to x,y,z. For controller.metrics['fspeed_pca'], controller.metrics['wavefrequency'], controller.metrics['amp']
-    fspeed_array = np.array([[controller[i].metrics['fspeed_PCA'], controller[i].metrics['wavefrequency'], controller[i].metrics['amp']] for i in range(len(controller))])
-    fspeed_labels = ['fspeed_PCA', 'wavefrequency', 'amp']
-    print(fspeed_array.shape)
-    print(fspeed_labels[0],'      ', fspeed_labels[1],'      ', fspeed_labels[2])
-    print(fspeed_array)
+        # make array n by 3 corresponding to x,y,z. For controller.metrics['fspeed_pca'], controller.metrics['wavefrequency'], controller.metrics['amp']
+        fspeed_array = np.array([[controller[i].metrics['fspeed_PCA'], controller[i].metrics['wavefrequency'], controller[i].metrics['amp']] for i in range(len(controller))])
+        fspeed_labels = ['fspeed_PCA', 'wavefrequency', 'amp']
+        print(fspeed_array.shape)
+        print(fspeed_labels[0],'      ', fspeed_labels[1],'      ', fspeed_labels[2])
+        print(fspeed_array)
 if __name__ == '__main__':
     exercise_multiple()
 
