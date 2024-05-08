99b44ffc370ec7ead0e574e871214f9f5d134a4d
AnteaCeko <92315155+AnteaCeko@users.noreply.github.com>
2024-05-08 14:35:24 +0200

    plotting the required muscle and CPG activities

--- Project_2/Python/exercise3.py
+++ Project_2/Python/exercise3.py
@@ -3,16 +3,60 @@ from util.run_open_loop import run_single
 from simulation_parameters import SimulationParameters
 import os
 import farms_pylog as pylog
+import numpy as np
+from plotting_common import *
+import matplotlib.pyplot as plt
 
 
-def exercise3():
+def exercise3(**kwargs):
 
     pylog.info("Ex 3")
     pylog.info("Implement exercise 3")
     log_path = './logs/exercise3/'
     os.makedirs(log_path, exist_ok=True)
 
+    pars_single = SimulationParameters(
+        n_iterations=5001,
+        log_path=log_path,
+        compute_metrics=3,
+        return_network=True,
+        **kwargs
+        )
+    
+    pylog.info("Running the simulation")
+    controller= run_single(pars_single)
+
+    pylog.info("Plotting the result")
+
+    left_idx = controller.muscle_l
+    right_idx = controller.muscle_r    
+    left_CPG = controller.all_v_left
+    right_CPG = controller.all_v_right
+
+    #Plot CPG activities
+    plt.figure('CPG_activities_single')
+    plot_left_right(
+        controller.times,
+        controller.state,
+        left_CPG,
+        right_CPG,
+        cm="jet",
+        offset=0.1)
+
+    #Plot MC activities
+    plt.figure('muscle_activities_single')
+    plot_left_right(
+        controller.times,
+        controller.state,
+        left_idx,
+        right_idx,
+        cm="jet",
+        offset=0.1)
+
+
+
 
 if __name__ == '__main__':
     exercise3()
+    plt.show()
 
