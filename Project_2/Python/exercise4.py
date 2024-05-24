18e56ce2555e2d719fef0bc6ed91346040398edb
AlexisCogne <alexis.cogne@outlook.com>
2024-05-24 18:40:49 +0200

    Adding implementations from Antea

--- Project_2/Python/exercise4.py
+++ Project_2/Python/exercise4.py
@@ -1,10 +1,56 @@
 
 
 from simulation_parameters import SimulationParameters
-from util.run_open_loop import run_multiple
+from util.run_closed_loop import run_multiple
 import numpy as np
 import farms_pylog as pylog
 import os
+import matplotlib.pyplot as plt
+from plotting_common import plot_metric_vs_parameter
+
+def collect_metrics(controllers, metric_key):
+    """
+    Collect the specified metric from a list of controllers.
+
+    Parameters:
+    - controllers: List of controllers
+    - metric_key: Key of the metric to collect
+
+    Returns:
+    - List or numpy array of collected metric values
+    """
+    metric_values = [controller.metrics[metric_key] for controller in controllers if metric_key in controller.metrics]
+
+    return metric_values
+def find_range_above_threshold(parameter_values, metric_values, threshold):
+    """
+    Find the range of parameter values where the metric is above the threshold.
+
+    Parameters:
+    - parameter_values: Values of the parameter
+    - metric_values: Values of the metric
+    - threshold: Threshold value
+
+    Returns:
+    - Tuple containing the start and end values of the range
+    """
+    start_index = None
+    end_index = None
+    for i, metric_value in enumerate(metric_values):
+        if metric_value > threshold:
+            if start_index is None:
+                start_index = i
+            end_index = i
+        else:
+            if start_index is not None:
+                break
+    if start_index is None:
+        return None, None
+    else:
+        return start_index, end_index, parameter_values[start_index], parameter_values[end_index]
+
+
+
 
 
 def exercise4():
@@ -12,9 +58,83 @@ def exercise4():
     pylog.info("Ex 4")
     pylog.info("Implement exercise 4")
     log_path = './logs/exercise4/'
+    save_path = '/Users/maxgrobbelaar/Documents/EPFL_Spring_2024/Computational Motor control/Project 2/figures/'
+
     os.makedirs(log_path, exist_ok=True)
+    
+    nsim = 20
+
+    pylog.info(
+        "Running multiple simulations in parallel from a list of SimulationParameters")
+    I_arr = np.linspace(0.05, 30, nsim)
+    pars_list = [
+        SimulationParameters(
+            simulation_i=i*nsim,
+            n_iterations=10001,
+            log_path="",
+            video_record=False,
+            compute_metrics=3,
+            I=I,
+            #b=b,
+            headless=True,
+            print_metrics=False,
+            return_network=True,
+        )
+        for i, I in enumerate(I_arr)
+        #for j, b in enumerate(np.linspace(8, 12, nsim))
+    ]
+
+    controllers = run_multiple(pars_list, num_process=8)
+    #print("Controller keys: ",controllers[0].metrics.keys())
+    # Call collect_metrics with the list of controllers and the desired metric key
+    metrics = collect_metrics(controllers, 'ptcc')
+
+    # Plot the collected metric values against I_arr
+    plot_metric_vs_parameter(I_arr, metrics, 'I', 'ptcc', save_path + 'ptcc_vs_I.png')
+
+    # Call the function with I_arr and metrics list, and specify the threshold
+    start_index, end_index, start_range, end_range = find_range_above_threshold(I_arr, metrics, 1.5)
+
+    # Print the range of I values where the metric is above 1.5
+    if start_range is not None and end_range is not None:
+        print(f"Range of I values where the metric is above 1.5: [{start_range}, {end_range}]")
+    else:
+        print("No range found where the metric is above 1.5")
+    '''
+    
+    start_range, end_range = 26.3, 26.6
+    nsim_r = 30
+    I_arr_range = np.linspace(start_range, end_range, nsim_r)
+    pars_list_range = [
+        SimulationParameters(
+            simulation_i=i*nsim_r,
+            n_iterations=10001,
+            log_path="",
+            video_record=False,
+            compute_metrics=3,
+            I=I,
+            #b=b,
+            headless=True,
+            print_metrics=False,
+            return_network=True,
+        )
+        for i, I in enumerate(I_arr_range)
+        #for j, b in enumerate(np.linspace(8, 12, nsim))
+    ]
+
+    controllers_range = run_multiple(pars_list_range, num_process=8)
+    # Extract the frequency and wavefrequency metrics for the range
+    frequency_values = [controller.metrics['frequency'] for controller in controllers_range]
+    print("Frequency values: ", frequency_values)
+    wavefrequency_values = [controller.metrics['wavefrequency'] for controller in controllers_range]
+    print("Wavefrequency values: ", wavefrequency_values)
+    # Plot the frequency against I_arr
+    plot_metric_vs_parameter(I_arr_range, frequency_values, 'I', 'Frequency', save_path + 'frequency_vs_I.png')
+
+    # Plot the wavefrequency against I_arr
+    plot_metric_vs_parameter(I_arr_range, wavefrequency_values, 'I', 'Wave Frequency', save_path + 'wavefrequency_vs_I.png')
+'''
 
 
 if __name__ == '__main__':
     exercise4()
-
--- Project_2/Python/exercise5.py
+++ Project_2/Python/exercise5.py
@@ -1,20 +1,253 @@
 
-
+from util.run_closed_loop import run_single
 from simulation_parameters import SimulationParameters
 from util.run_closed_loop import run_multiple
+from plotting_common import  plot_trajectory, plot_left_right,plot_metric_vs_parameter
+from exercise4 import collect_metrics
 import numpy as np
 import farms_pylog as pylog
 import os
+import matplotlib.pyplot as plt
 
 
-def exercise5():
 
-    pylog.info("Ex 5")
-    pylog.info("Implement exercise 5")
+
+def exercise5_performance(**kwargs):
+    """
+    This functions runs the simulation for default parameters, allowing to visualize the swimming behavior and to test its performance.
+    """
+    log_path = './logs/exercise5/'  # path for logging the simulation data
+    os.makedirs(log_path, exist_ok=True)
+
+    all_pars = SimulationParameters(
+        n_iterations=5001,
+        log_path=log_path,
+        compute_metrics=3,
+        return_network=True,
+        **kwargs
+    )
+
+    pylog.info("Running the simulation")
+    controller = run_single(
+        all_pars
+    )
+    #First of all we observe the fish swimming
+
+    pylog.info("Plotting the result")
+
+    left_idx = controller.muscle_l
+    right_idx = controller.muscle_r
+
+    # example plot using plot_trajectory
+    plt.figure("trajectory_single")
+    plot_trajectory(controller)
+
+    #Printing the performance associated to the default parameters
+    print(" ")
+    print("-----------------------")
+    print("Performance summary")
+    print("-----------------------")
+    print(" ")
+
+    speed = controller.metrics["fspeed_cycle"]
+    torque = controller.metrics["torque"]
+    lateral_speed = controller.metrics["lspeed_cycle"]
+    ptcc = controller.metrics["ptcc"]
+    
+    print("best speed achieved: ", "{:.2e}".format(speed))
+    print("lowest torque achieved: ", "{:.2e}".format(torque))
+    print("lowest lateral speed achieved: ", "{:.2e}".format(lateral_speed))
+    print("ptcc max achieved: ", "{:.2e}".format(ptcc))
+    #
+    print(f'Parameters: frequency={np.round(0.5*controller.metrics["frequency"],5)}, Amp={np.round(0.5*controller.metrics["amp"],5)}, wavefrequency={np.round(controller.metrics["wavefrequency"],5)}')
+
+
+
+
+def exercise5_turning_performance(**kwargs):
+    """
+    Function used to vary I_diff and measure the associated curvature and lateral speed.
+    """
+    log_path = './logs/exercise5/'  # path for logging the simulation data
+    os.makedirs(log_path, exist_ok=True)
+
+    nsim = 8
+
+    # Define the range of Idiff values to test
+    Idiff_range = np.linspace(0, 4, nsim)
+    
+    par_list = [
+        SimulationParameters(
+            simulation_i=idiff*nsim,  
+            n_iterations=3000,  
+            log_path=log_path,
+            video_record=False,  
+            compute_metrics=3,  
+            Idiff=Idiff,  
+            headless=True,  
+            print_metrics=False,
+            return_network=True, 
+            **kwargs
+            )
+        for idiff, Idiff in enumerate(Idiff_range)
+    ]
+    
+    # Plot the interesting metric values against Idiff
+
+    controllers = run_multiple(par_list, num_process=8)
+    #lateral speed cycle
+    lspeed_values = [controller.metrics["lspeed_cycle"] for controller in controllers]
+    print("Lateral speed cycle values: ", lspeed_values)
+    plot_metric_vs_parameter(Idiff_range, lspeed_values, 'Idiff', "lspeed_cycle")
+    #lateral speed PCA
+    lspeed_values_PCA = [controller.metrics["lspeed_PCA"] for controller in controllers]
+    print("Lateral speed PCA values: ", lspeed_values_PCA)
+    plot_metric_vs_parameter(Idiff_range, lspeed_values_PCA, 'Idiff', "lspeed_PCA")
+    #curvature
+    curv_values = [controller.metrics['curvature'] for controller in controllers]
+    print("Curvature values: ", curv_values)
+    plot_metric_vs_parameter(Idiff_range, curv_values, 'Idiff', "curvature")
+    return Idiff_range, lspeed_values, lspeed_values_PCA, curv_values
+
+
+
+def exercise5_radius_check(fig, Idiffs, **kwargs):
+    """
+    Function used to plot the turning performance using the curvature.
+    """
+    pylog.info("Ex 5: Turning Performance Analysis")
     log_path = './logs/exercise5/'
     os.makedirs(log_path, exist_ok=True)
 
+    # Define parameters for the simulation runs
+    par_list = [
+        SimulationParameters(
+            simulation_i=idiff * len(Idiffs),
+            n_iterations=5001,
+            log_path=log_path,
+            compute_metrics=3,
+            Idiff=Idiff,
+            headless=True,
+            print_metrics=False,
+            return_network=True,
+            **kwargs
+        )
+        for idiff, Idiff in enumerate(Idiffs)
+    ]
+
+    # Run the simulations
+    controllers = run_multiple(par_list, num_process=4)
+
+    # Extract curvature metric
+    curv_values = [controller.metrics['curvature'] for controller in controllers]
+    
+    #Compute the turning radius from the curvature metric
+    for i, controller in enumerate(controllers):
+        curvature = curv_values[i]
+        turning_radius = compute_turning_radius(curvature)
+
+        plt.figure(f"Trajectory_Idiff_{Idiffs[i]}")
+        title_text = (f'Turning radius VS center of mass trajectory\n'
+                      f'Idiff={Idiffs[i]}, '
+                      f'Curvature (--)={{curvature:.3f}}, '
+                      f'Turning Radius (--)={{turning_radius:.3f}}').format(
+                      curvature=curvature, turning_radius=turning_radius)
+        plot_trajectory(controller, label=f'Idiff={Idiffs[i]}',
+                        title=title_text)
+        #plot_trajectory(controller, label=f'Idiff={Idiffs[i]}',
+        #                title=f'Turning radius VS center of mass trajectory\nIdiff={Idiffs[i]}, Curvature(--)={curvature:.3f}, Turning Radius (--)={turning_radius:.3f}')
+
+        # Plot the turning radius as a circle
+        if turning_radius != float('inf'):
+            plot_turning_radius(controller, turning_radius)      #plot the turning radius obtained from the curvature computation
+
+        plt.legend(loc='lower right')
+        plt.grid(True)
+
+    plt.show()
+    return curv_values
+
+def compute_turning_radius(curvature):
+    """
+    Function computing the turning radius from the curvature.
+    """
+    if curvature != 0:
+        turning_radius = 1 / curvature
+    else:
+        turning_radius = float('inf')  # Straight line, infinite turning radius
+    return np.abs(turning_radius)
+
+
+def plot_turning_radius(controller, turning_radius):
+    """
+    PFunction plotting the turning radius on the trajectory plot.
+    """
+    head_positions = np.array(controller.links_positions)[:, 0, :]
+    center_of_mass = np.mean(head_positions[:, :2], axis=0)
+
+    circle = plt.Circle(center_of_mass, turning_radius, color='r', fill=False, linestyle='--', label='Turning Radius [m]')
+    plt.gca().add_patch(circle)
+
+
+
+
+
+def plot_swimming_behavior(specific_Idiff):
+    """Plots the swimming trajectory and the CPG activations based on the chosen Idiff. """
+
+    pylog.info("Implement exercise 5 swimming")
+    log_path = './logs/exercise5/'
+    os.makedirs(log_path, exist_ok=True)
+
+    pars = SimulationParameters(
+        n_iterations=5001,
+        log_path=log_path,
+        compute_metrics=3,
+        return_network=True,
+        Idiff=specific_Idiff,
+        headless=False)
+    
+    pylog.info("Running the simulation")
+    controller= run_single(pars)
+
+    pylog.info("Plotting the result")
+
+    left_idx = controller.muscle_l
+    right_idx = controller.muscle_r    
+    left_CPG = controller.all_v_left
+    right_CPG = controller.all_v_right
+
+    # example plot using plot_trajectory
+    plt.figure("trajectory_single")
+    plot_trajectory(controller)
+
+    #Plot CPG activities
+    plt.figure('CPG_activities_single')
+    plot_left_right(
+        controller.times,
+        controller.state,
+        left_CPG,
+        right_CPG,
+        cm="jet",
+        offset=0.1)
+
 
 if __name__ == '__main__':
-    exercise5()
 
+    #-----run the simulation for default parameters, allowing to visualize the swimming behavior and to test its performance
+    #exercise5_performance(headless=False)
+
+    #-----test different Idiffs and obtain the performance: lateral speeds and curvatures associated
+    #exercise5_turning_performance()
+
+    #-----check that turning radius match curvature expected
+    figname="ex5"
+    fig = plt.figure(figname)
+    Idiffs= np.linspace(0, 4, 10)
+    #exercise5_radius_check(fig, Idiffs)
+
+    #-----plot swimming trajectory and neuron activities
+    plot_swimming_behavior(specific_Idiff=0)
+    plot_swimming_behavior(specific_Idiff = 4)
+
+    plt.show()
